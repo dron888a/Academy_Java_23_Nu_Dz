0bc4203ccd7ed356cad8867b7bc7401ea1e344b0
Александр dron888a <dron888a@gmail.com>
2024-06-10 00:58:41 +0800

    H11 - Работа, над ошибками

--- src/ru/progwards/java1/lessons/wrappers/StringConverter.java
+++ src/ru/progwards/java1/lessons/wrappers/StringConverter.java
@@ -24,57 +24,67 @@ public class StringConverter {
     public static String fromDouble(double number) {
         return Double.toString(number);
     }
+
+    public static void main(String[] args) {
+        System.out.println(42);
+        System.out.println(15);
+        System.out.println(12);
+        System.out.println(454545);
+        System.out.println(5.0f);
+        System.out.println(8.0);
+    }
 }
 
 class NumMetrics {
 
     public static Integer sumDigits(Integer number) {
         Integer int1 = number / 100;
-        Integer int2 = (number % 100) /10;
+        Integer int2 = (number % 100) / 10;
         Integer int3 = number % 10;
         return int1 + int2 + int3;
     }
 
     public static Integer mulDigits(Integer number) {
         Integer int1 = number / 100;
-        Integer int2 = (number % 100) /10;
+        Integer int2 = (number % 100) / 10;
         Integer int3 = number % 10;
         return int1 * int2 * int3;
     }
 
-public class AccuracyDoubleFloat {
+    public static void main(String[] args) {
+        System.out.println(NumMetrics.sumDigits(456));
+        System.out.println(NumMetrics.mulDigits(456));
+    }
+}
+
+class AccuracyDoubleFloat {
 
     public static Double volumeBallDouble(Double radius) {
-        double pi = 3.14;
-        return (4.0 / 3.0) * pi * (radius * radius * radius);
+        double Pi = 3.14;
+        Double volumeDoubble = (4.0 / 3.0) * Pi * (radius * radius * radius); // Вычисление объема шара
+        return volumeDoubble; // Возвращение объема
     }
 
     public static Float volumeBallFloat(Float radius) {
-        float pi = 3.14f;
-        return (4.0f / 3.0f) * pi * (radius * radius * radius);
+        float Pi = 3.14f;
+        Float volumeFloat = (4.0f / 3.0f) * Pi * (float) (radius * radius * radius); // Вычисление объема шара
+        return volumeFloat; // Возвращение объема
     }
 
     public static Double calculateAccuracy(Double radius) {
         Double volumeDouble = volumeBallDouble(radius);
-        Float radiusFloat = radius.floatValue();
-        Float volumeFloat = volumeBallFloat(radiusFloat);
-
+        Float volumeFloat = volumeBallFloat(radius.floatValue());
         return volumeDouble - volumeFloat;
     }
-}
 
     public static void main(String[] args) {
+        Double radius = 6371.2;
+        Double volumeD = volumeBallDouble(radius);
+        Float volumeF = volumeBallFloat(radius.floatValue());
+        Double accuracy = calculateAccuracy(radius);
 
-        System.out.println(StringConverter.fromByte((byte) 42));
-        System.out.println(StringConverter.fromShort((short) 15));
-        System.out.println(StringConverter.fromInt(12));
-        System.out.println(StringConverter.fromLong(454545));
-        System.out.println(StringConverter.fromFloat(5.0f));
-        System.out.println(StringConverter.fromDouble(8.0));
-        System.out.println(NumMetrics.sumDigits(456));
-        System.out.println(NumMetrics.mulDigits(456));
-        System.out.println(AccuracyDoubleFloat.volumeBallDouble(6371.2));
-        System.out.println(AccuracyDoubleFloat.volumeBallFloat(6371.2f));
-        System.out.println(AccuracyDoubleFloat.calculateAccuracy(6371.2));
+        System.out.println("Объем шара с радиусом " + radius + " км (Double): " + volumeD + " куб. км");
+        System.out.println("Объем шара с радиусом " + radius + " км (Float): " + volumeF + " куб. км");
+        System.out.println("Разница между Double и Float: " + accuracy + " куб. км");
     }
 }
