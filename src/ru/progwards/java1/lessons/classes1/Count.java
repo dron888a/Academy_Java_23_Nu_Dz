e278dc8fdb455afc081584e91e3cf463accee82e
Александр dron888a <dron888a@gmail.com>
2024-07-30 11:41:12 +0800

    H15

--- src/ru/progwards/java1/lessons/classes1/Count.java
+++ src/ru/progwards/java1/lessons/classes1/Count.java
@@ -2,9 +2,9 @@ package ru.progwards.java1.lessons.classes1;
 
 public class Count {
 
-    int count;
+    private int count;
 
-    public Count () {
+    public Count() {
         count = 0;
     }
 
@@ -33,7 +33,40 @@ public class Count {
                 System.out.println("count равен 0");
                 break;
             }
-
         }
+    }
+}
+
+class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    public int toSeconds() {
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public int secondsBetween(Time time) {
+        int thisTimeInSeconds  = this.toSeconds();
+        int otherTimeInSeconds  = time.toSeconds();
+        return Math.abs(thisTimeInSeconds - otherTimeInSeconds);
+    }
+
+    public static void main(String[] args) {
+        Time time1 = new Time(1, 30, 5);
+        Time time2 = new Time(1, 35, 10);
+        System.out.println(time1);
+        System.out.println(time1.toSeconds());
+        System.out.println(time1.secondsBetween(time2));
     }
 }
